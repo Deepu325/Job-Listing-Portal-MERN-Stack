8fb1103b31d4d99ecd5423a69bf2a57a1e832774
Arkasmit Sengupta <senguptaarkasmit@gmail.com>
2026-01-06 16:36:45 +0530

    Add frontend with login and register pages

---
+++ Frontend/src/components/ui/authentication/Register.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Navbar from '../components_lite/Navbar'
+import { Label } from '../label'
+import { Input } from '../input'
+import { RadioGroup, RadioGroupItem } from '../radio-group'
+import { Link } from 'react-router-dom'
+
+const Register = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className='flex items-center justify-center max-w-7xl mx-auto'>
+        <form action="" className='w-1/2 border border-grey-500 rounded-md p-4 my-10'>
+          <h1 className='font-bold text-xl mb-5 text-center text-green-900'>Register</h1>
+          <div className=' my-2'>
+            <Label>Name</Label>
+            <Input type='text' placeholder='Sam Walker'></Input>
+          </div>
+          <div className=' my-2'>
+            <Label>Email</Label>
+            <Input type='email' placeholder='samWalker@gmail.com'></Input>
+          </div>
+          <div className=' my-2'>
+            <Label>Phonne Number</Label>
+            <Input type='tel' placeholder='+1234567890'></Input>
+          </div>
+          <div className=' my-2'>
+            <Label>Password</Label>
+            <Input type='password' placeholder='********'></Input>
+          </div>
+          <div className='flex item-center justify-between'>
+            
+            <RadioGroup className='flex items-center gap-4 my-5'>
+              <div className="flex items-center space-x-2">
+                <Input type='radio' name='role' value='Student' className='cursor-pinter' />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                 <Input type='radio' name='role' value='Recruiter' className='cursor-pinter' />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <button className='block w-full py-3 my-3 text-white bg-lime-700 hover:bg-primary/90 rounded-md'>
+            Register
+          </button>
+          {/* already account then login */}
+          <p className='text-green-700 text-md my-2'>
+            Already have an acount? 
+            <Link to="/login" className='text-blue-950'>Login</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
