0574ad0ab5f424b5622535b4fe4e7b2eac0927a7
Arkasmit Sengupta <senguptaarkasmit@gmail.com>
2026-01-06 16:36:45 +0530

    Add frontend with login and register pages

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+ import { Button } from "@/components/ui/button"
+import Navbar from "./components/ui/components_lite/Navbar"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Login from "./components/ui/authentication/Login";
+import  Register from "./components/ui/authentication/Register"
+import Home from "./components/ui/components_lite/Home";
+
+
+
+// creating browser based router
+const appRouter = createBrowserRouter([ //configuration based routing(Array of routes)
+  {path: "/",
+    element: <Home/>
+  },
+  {
+    path:"/login",
+    element: <Login/>
+  },
+  {
+    path:"/register",
+    element: <Register/>
+  },
+]);
+
+function App() {
+  return (
+    <div>
+        <RouterProvider router = {appRouter}></RouterProvider>
+    </div>
+  )
+}
+
+export default App
