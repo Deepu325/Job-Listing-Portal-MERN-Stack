83fbb0a0b97da881bf4e97067d4586adcb35ac84
deepu325 <deepukc2526@gmail.com>
2026-01-08 21:06:06 +0530

    Resolve final conflicts and verify build

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,36 +1,21 @@
-<<<<<<< HEAD
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-=======
 import ProtectedRoute from "./routes/ProtectedRoute";
-import { Button } from "@/components/ui/button"
 import Navbar from "./components/ui/components_lite/Navbar"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
->>>>>>> origin/frontend-auth-context
 import Login from "./components/ui/authentication/Login";
 import Register from "./components/ui/authentication/Register";
 import Home from "./components/ui/components_lite/Home";
-
-<<<<<<< HEAD
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-=======
 
 
 // creating browser based router
 const appRouter = createBrowserRouter([ //configuration based routing(Array of routes)
   {
-  path: "/",
-  element: (
-    <ProtectedRoute>
-      <Home />
-    </ProtectedRoute>
-  )
-},
-
->>>>>>> origin/frontend-auth-context
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    )
+  },
   {
     path: "/login",
     element: <Login />,
